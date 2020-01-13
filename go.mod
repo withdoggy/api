4c77ebc52145559eb01d706c22e4fc0415226212
Wojciech Wójcik <wojtaswojcik@gmail.com>
2020-01-13 17:42:56 +0100

    add tracing

--- go.mod
+++ go.mod
@@ -6,9 +6,11 @@ require (
 	cloud.google.com/go v0.46.3
 	cloud.google.com/go/datastore v1.0.0
 	cloud.google.com/go/firestore v1.1.1
+	contrib.go.opencensus.io/exporter/stackdriver v0.12.8
 	firebase.google.com/go v3.11.1+incompatible
 	github.com/gin-gonic/gin v1.5.0
 	github.com/spf13/cobra v0.0.5
+	go.opencensus.io v0.22.1
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
 	google.golang.org/api v0.14.0
 )
--- petsmanagement/cmd/api.go
+++ petsmanagement/cmd/api.go
@@ -4,16 +4,30 @@ import (
 	"fmt"
 	"os"
 
+	"contrib.go.opencensus.io/exporter/stackdriver"
 	"github.com/spf13/cobra"
 	"github.com/withdoggy/api/petsmanagement/db"
 	"github.com/withdoggy/api/petsmanagement/http"
+	"go.opencensus.io/trace"
 )
+
+const projectID = "withdoggy"
 
 var apiCmd = &cobra.Command{
 	Use:   "api",
 	Short: "Run api server",
 	Run: func(cmd *cobra.Command, args []string) {
-		d, err := db.NewFirestoreClient("withdoggy")
+
+		sd, err := stackdriver.NewExporter(stackdriver.Options{
+			ProjectID: projectID,
+		})
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer sd.Flush()
+		trace.RegisterExporter(sd)
+		d, err := db.NewFirestoreClient(projectID)
 		defer d.Close()
 		if err != nil {
 			fmt.Println(err)
